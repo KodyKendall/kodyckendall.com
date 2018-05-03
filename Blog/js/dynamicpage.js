9784c86e8ea6e242bee41448dcd12ea440251926
KodyKendall <kodyckendall@gmail.com>
2018-05-03 17:23:01 -0600

    Latest commits.

--- Blog/js/dynamicpage.js
+++ Blog/js/dynamicpage.js
@@ -1,13 +1,27 @@
-
 var localDevMode = true;
 var relativePath = ""
 
+function GenerateBlogPage(blogJson){
+	/*blogJson contains: 
+	- text (as a WYSIWYG)
+	- author
+	- date
+	- title
+	- imgLink
+	- subtitle
+	*/
+
+	var page = $.template(
+			
+	)
+}
+
 function GetPath(){
-	if (localDevMode){
-		relativePath = "http://localhost:31338"
-		return(relativePath);
-	}
-	else{
-		return(relativePath);		
-	}
-};
+    if (localDevMode){
+        relativePath = "http://localhost:31338"
+        return(relativePath);
+    }
+    else{
+        return(relativePath);		
+    }
+} 
